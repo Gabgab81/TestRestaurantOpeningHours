5fb9612b8b26041e14bc03a50241364c356ef78a
gabriel roux <rouxgab81@gmail.com>
2022-11-27 11:01:28 -0500

    edit Done

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :restaurants, only: [:index, :show, :new, :create, :destroy]
+  resources :restaurants, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
 end
