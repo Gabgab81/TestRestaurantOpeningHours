40a59ba10a1aacb68c812084f9f70981d3ddd8aa
gabriel roux <rouxgab81@gmail.com>
2022-11-29 18:18:52 -0500

    add validation for am_closes_at and pm_opens_at

--- app/helpers/restaurant_helper.rb
+++ app/helpers/restaurant_helper.rb
@@ -1,13 +1,7 @@
 module RestaurantHelper
-    def weekdays
-        [
-            {label: "Monday", value: 1},
-            {label: "Tuesday", value: 2},
-            {label: "Wednesday", value: 3},
-            {label: "Thursday", value: 4},
-            {label: "Friday", value: 5},
-            {label: "Saturday", value: 6},
-            {label: "Sunday", value: 0},
-        ]
+    def invalid?(attribute, day)
+        @restaurant.errors.include?(attribute)  && 
+        !params[:restaurant].nil? && 
+        params[:restaurant][:schedules_attributes][day[:value] == 0 ? "6" : (day[:value] - 1).to_s][:_destroy] == "0"
     end
 end
