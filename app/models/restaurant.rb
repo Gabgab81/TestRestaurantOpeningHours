fcc6610b90ca6355c2e4c8f4f594aea19fbaee3f
gabriel roux <rouxgab81@gmail.com>
2022-11-29 15:15:39 -0500

    create whenOpen for restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -29,36 +29,51 @@ class Restaurant < ApplicationRecord
     end
 
     def whenOpen
-        todayOpen = schedules.where(weekday: Time.now.wday)
-        if todayOpen.empty?
-            day = Time.now.wday + 1
-            while schedules.where(weekday: day).empty?
-                day == 6 ? day = 0 : day += 1
-            end
-        else
-            amOpen = todayOpen.first.am_opens_at.strftime("%H:%M")
-            pmOpen = todayOpen.first.pm_opens_at.nil? ? nil : todayOpen.first.pm_opens_at.strftime("%H:%M")
-        end
-        time = Time.now.strftime("%H:%M")
-        if !todayOpen.empty? && (amOpen > time || (!pmOpen.nil? && pmOpen > time)) 
-            if amOpen > time
-                openHour = /(\d*):(\d*)/.match(todayOpen.first.am_opens_at.strftime("%H:%M"))
-                dopenHour = openHour[1].to_i * 3600 + openHour[2].to_i * 60
-                time = /(\d*):(\d*)/.match(time)
-                durationTime = time[1].to_i * 3600 + time[2].to_i * 60
-                dopenHour - durationTime
+        if !self.open?
+            todayOpen = schedules.where(weekday: Time.now.wday)
+            time = Time.now.strftime("%H:%M")
+            if !todayOpen.empty?
+                amOpen = todayOpen.first.am_opens_at.strftime("%H:%M")
+                pmOpen = todayOpen.first.pm_opens_at.nil? ? nil : todayOpen.first.pm_opens_at.strftime("%H:%M")
+                pmClose = todayOpen.first.pm_closes_at.strftime("%H:%M")
+                if pmClose < time
+                    nday, day, amOpen = ndayDayAmopen
+                end
             else
-                openHour = /(\d*):(\d*)/.match(todayOpen.first.pm_opens_at.strftime("%H:%M"))
-                dopenHour = openHour[1].to_i * 3600 + openHour[2].to_i * 60
-                time = /(\d*):(\d*)/.match(time)
-                durationTime = time[1].to_i * 3600 + time[2].to_i * 60
-                dopenHour - durationTime
+                nday, day, amOpen = ndayDayAmopen
             end
-            # p amOpen, pmOpen, time
+            timeMached = /(\d*):(\d*)/.match(time)
+            durationTime = timeMached[1].to_i * 3600 + timeMached[2].to_i * 60
+            if !todayOpen.empty? && (amOpen > time || (!pmOpen.nil? && pmOpen > time)) 
+                if amOpen > time
+                    openHourSecond(amOpen) - durationTime
+                else
+                    openHourSecond(amOpen) - durationTime
+                end
+            else
+                nday * 3600 * 24 - durationTime + openHourSecond(amOpen)
+            end
         else
-            # p Time.now.wday, schedules.where(weekday: Time.zone.now.wday).first.weekday
-            day
+            0
         end
+    end
+
+    private
+
+    def openHourSecond(amOpen)
+        openHour = /(\d*):(\d*)/.match(amOpen)
+        dopenHour = openHour[1].to_i * 3600 + openHour[2].to_i * 60
+    end
+
+    def ndayDayAmopen
+        nday = 1
+        day = Time.now.wday + 1
+        while schedules.where(weekday: day).empty? && Time.now.wday != day
+            nday += 1
+            day == 6 ? day = 0 : day += 1
+        end
+        amOpen = schedules.where(weekday: day).first.am_opens_at.strftime("%H:%M")
+        return nday, day, amOpen
     end
 
 end
